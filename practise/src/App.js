96e0e00320260e8dca1e31ef125cb1b4eaa15932
eda <edaburns@gmail.com>
2022-09-13 12:11:33 +0100

    added a remove and toggle function

--- practise/src/App.js
+++ practise/src/App.js
@@ -1,20 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+// const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
 function App() {
   const [todos, setTodos] = useState ([]); 
+
+  // useEffect(() => {
+  //   const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (storageTodos) {
+  //     setTodos(storageTodos);
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem (LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  // }, [todos]);
+  
 
   function addTodo(todo) {
     setTodos([todo, ...todos]);
   }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id ===id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !==id));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>React Todo</p>
+        <p>React Todo List</p>
         <TodoForm addTodo={addTodo}/>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo ={removeTodo}/>
       </header>
     </div>
   );
--- practise/src/components/Todo.js
+++ practise/src/components/Todo.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
-function Todo({ todo }) {
+function Todo({ todo, toggleComplete, removeTodo }) {
+    function handleCheckboxClick(){
+        toggleComplete(todo.id)
+    }
+    function handleRemoveClick () {
+        removeTodo(todo.id);
+    }
     return (
         <div style= {{ display: "flex" }}>
-        <input type="checkbox" />
-        <li 
+        {/* flex makes the list items display horizontally */}
+        <input type="checkbox" onClick={handleCheckboxClick}/>
+        <ul
         style={{
             color:"white",
             textDecoration: todo.completed ? "line-through" : null        }}
             >
+            {/* ternary operator ? is same as if-else statement */}
             {todo.task}
-            </li>
-        <button>X</button>
+            </ul>
+        <button onClick={handleRemoveClick}> REMOVE </button>
         </div>
     );
 }
